919787cb475987dd2d3c9e5c06db10bb836c2a22
Tony Wang <pwang009@hotmail.com>
2020-09-06 16:41:01 -0700

    minor mistake

--- scripts/test_container.sh
+++ scripts/test_container.sh
@@ -2,5 +2,5 @@ count=0
 echo "$env:STAGE_NAME"
 
 container_url=http://localhost:7080
-ret=curl -s -I -o /dev/null -w '%{http_code}' $container_url
+ret=$(curl -s -I -o /dev/null -w '%{http_code}' $container_url)
 echo $ret
